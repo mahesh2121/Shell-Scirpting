6dd73a8baf91e31c83f8725b68faacb69880e5eb
mahesh2121 <maheshgaikwad128@gmail.com>
2023-05-29 01:25:57 +0530

    arithemethic

---
+++ advanced/bash4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+num1=20
+num2=20
+echo $(( num1 + num2 ))
+echo $(( num1 - num2 ))
+echo $(( num1 * num2 ))
+echo $(( num1 / num2 ))
+echo $(( num1 % num2 ))
+
+echo "====================================with expre statement==========================="
+
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
